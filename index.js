1aca8e29910a0552e4c7062e2c9dae704954f588
황현준 <101453046+hwanghj09@users.noreply.github.com>
2025-07-30 18:28:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { v4: uuidv4 } = require('uuid');
 
 const io = new Server(server, {
   path: '/socket.io/',
-  transports: ['websocket'], // Force websocket transport
   cors: {
     origin: "*", // Allow all origins for development. Restrict in production.
     methods: ["GET", "POST"]
